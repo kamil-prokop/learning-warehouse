bcfb98454b0785dc1d956b04c60525b7c4a6b790
Kamil Prokop <zara.pug.yt@gmail.com>
2023-01-18 01:49:58 +0100

    New option

--- warehouse_manager.py
+++ warehouse_manager.py
@@ -2,7 +2,7 @@ items = {
     "name": ["Milk", "Sugar", "Flour", "Coffee"],
     "quantity": [],
     "unit": [],
-    "unit_price": []
+    "unit_price PLN": []
 }
 
 
@@ -11,5 +11,9 @@ def intro_question():
     init_answer = input("What would you like to do? ")
     if init_answer == "exit":
         print("Exiting... Bye!")
+    
+    for k, v in items.items():
+        text = k
+        print(text)
 
 intro_question()
