fc6b041da044008437dfa695cab10eb09544bc69
Kamil Prokop <zara.pug.yt@gmail.com>
2023-01-18 01:40:08 +0100

    initial products

--- warehouse_manager.py
+++ warehouse_manager.py
@@ -1,5 +1,15 @@
-def intro_question(X_replay):
-    init_answer = input("What would you like to do?")
+items = {
+    "name": ["Milk", "Sugar", "Flour", "Coffee"],
+    "quantity": [],
+    "unit": [],
+    "unit_price": []
+}
+
+
+def intro_question():
+    #X_replay == str(init_answer)
+    init_answer = input("What would you like to do? ")
     if init_answer == "exit":
         print("Exiting... Bye!")
 
+intro_question()
